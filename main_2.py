ca0482ff6fe98f4216a26b8314457b0d3090e757
cafferatielisa <cafferati.elisa@iisgalvanimi.edu.it>
2022-10-20 10:45:24 +0000

    girono 2

---
+++ main_2.py
@@ -0,0 +1,4 @@
+frutta = ["apple", "banana", "cherry"] #crea una lista. nella lista posso mettere tutti i tipi di variabile
+print(frutta) #stampa tutto, compreso parentesi quadre e virgolette
+print(len(frutta)) #mi dice quanti componenti ho nella lista
+print(type(frutta)) #mi dice la classe della variabile, in questo caso è una lista

c2813d6b89a973ca6b484c6d78f359521068104c
cafferatielisa <cafferati.elisa@iisgalvanimi.edu.it>
2022-10-31 17:59:28 +0000

    giorno 4

---
+++ main_6.py
@@ -0,0 +1,4 @@
+#Controlla se il numero 5 è un numero intero
+#La isinstance()funzione restituisce Truese l'oggetto specificato è del tipo specificato, altrimenti False
+#isinstance(object, type)
+x = isinstance(5, int) 

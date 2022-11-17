52499c93da304ae9c22721fa08eb164ea8047cb1
cafferatielisa <cafferati.elisa@iisgalvanimi.edu.it>
2022-11-17 11:51:25 +0000

    esercizi

---
+++ 2prep.py
@@ -0,0 +1,30 @@
+#esercizio 1
+
+#for x in range(5):
+#   y = input("Dimmi un numero: ")
+#   print(y)
+
+#esercizio 2
+#num=[]
+#for x in range(5):
+#   y = input("Dimmi un numero: ")
+#   num.append(int(y))
+#num.reverse()
+#print(num)
+
+#esercizio 3
+#numero6,numero7,numero8,numero9,numero10=input("dimmi un numero"), input(), input(),input(),input()
+#numeri=[int(numero6),numero7,numero8,numero9,numero10]
+
+#num = []
+#for x in range(5):
+#    y = input("Dimmi un numero")
+#    num.append(int(y))
+#numeripari=[]
+#for n in num:
+#    check= int(n/2)
+#    if (check*2)==n:
+#        numeripari.append(n)
+#print(numeripari)
+
+#esercizio 4
